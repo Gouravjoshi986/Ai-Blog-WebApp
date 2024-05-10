9a23bf6de5425a57d256cd9f0b8e1ba1c10a0401
Gouravjoshi986 <gourav.joshi2003@gmail.com>
2024-05-10 16:35:56 +0530

    Other Component completed excluding images

--- ailog/app/(Shared)/Other.tsx
+++ ailog/app/(Shared)/Other.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Card from "./Card";
+import {Post} from '@prisma/client'
 
 type Props = {
+  otherPosts:Array<Post>
 };
 
-const Other = () => {
+const Other = ({otherPosts}:Props) => {
   return (
     <section className="pt-4 mb-16">
       <hr className="border-1" />
@@ -14,18 +16,22 @@ const Other = () => {
         <Card
           className="mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={otherPosts[0]}
         />
         <Card
           className="mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={otherPosts[1]}
         />
         <Card
           className="mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={otherPosts[2]}
         />
         <Card
           className="mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={otherPosts[3]}
         />
       </div>
     </section>
