f1c64120b42b37dcd05c579e137e9062b337b522
Gouravjoshi986 <gourav.joshi2003@gmail.com>
2024-05-10 17:58:25 +0530

    Added Content Component to individual pages

--- ailog/app/post/[id]/page.tsx
+++ ailog/app/post/[id]/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Post as PostType } from "@prisma/client";
 import { FormattedPost } from "../../types";
 import Sidebar from "@/app/(shared)/Sidebar";
+import Content from './Content';
 
 type Props = {
   params: { id: string };
@@ -41,7 +42,7 @@ const Post = async ({ params }: Props) => {
     <main className="px-10 leading-7">
       <div className="md:flex gap-10 mb-5">
         <div className="basis-3/4">
-          {/* <Content post={post} /> */}content component from ai
+          <Content post={post} />
         </div>
         <div className="basis-1/4">
           <Sidebar />
